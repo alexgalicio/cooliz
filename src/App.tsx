3f53964da1f55e20f1fddb435e2c2129bf164abe
Alex Galicio <alexisjoygalicio@gmail.com>
2026-01-28 08:54:39 +0800

    feat: add pages

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Payments from "./pages/Payments";
+import SalesReport from "./pages/SalesReport";
+import Bookings from "./pages/Bookings";
+import Dashboard from "./pages/Dashboard";
+import NewBooking from "./pages/NewBooking";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/bookings' element={<Bookings />} />
+        <Route path='/newBooking' element={<NewBooking />} />
+        <Route path='/payments' element={<Payments />} />
+        <Route path='/salesReport' element={<SalesReport />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
