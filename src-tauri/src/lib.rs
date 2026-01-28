b077e39e9a939de33cfdd5ed3674e52f200e77cb
Alex Galicio <alexisjoygalicio@gmail.com>
2026-01-28 08:53:26 +0800

    feat: add sql connection

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,30 @@
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use tauri_plugin_sql::{Migration, MigrationKind};  
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    let migrations = vec![  
+        Migration {  
+            version: 1,  
+            description: "initial migration",  
+            sql: include_str!("migration/1.sql"), 
+            kind: MigrationKind::Up,  
+        }  
+    ]; 
+
+    tauri::Builder::default()
+        .plugin(
+            tauri_plugin_sql::Builder::default()
+                .add_migrations("sqlite:cooliz.db", migrations)
+                .build()
+        )
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
